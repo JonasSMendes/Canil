0d4fb99d48d51cb0f9af8aa6e256e3c014ee2434
jonas mendes <jonas.jsm2903@gmail.com>
2023-07-29 13:02:49 -0300

    criando telas

--- src/controllers/pageControler.ts
+++ src/controllers/pageControler.ts
@@ -2,17 +2,34 @@ import { Request , Response } from "express";
 
 export const home = (req:Request, res:Response) =>{
 
-    res.render('pages/home')
+    res.render('pages/page', {
+        banner:{
+            title: 'todos os animais',
+            background:'allanimals.jpg'
+        }
+    })
 }
 export const dog = (req:Request, res:Response) =>{
 
-    res.render('pages/dog')
+    res.render('pages/page', {
+         banner:{
+             title: 'Cachorros',
+             background:'banner_dog.jpg' }
+         })
 }
 export const cat = (req:Request, res:Response) =>{
 
-    res.render('pages/cat')
+    res.render('pages/page', {
+        banner:{
+            title: 'Gatos',
+            background:'banner_cat.jpg' }
+        })
 }
 export const fish = (req:Request, res:Response) =>{
 
-    res.render('pages/fish')
+    res.render('pages/page', {
+        banner:{
+            title: 'Peixes',
+            background:'banner_fish.jpg' }
+        })
 }
